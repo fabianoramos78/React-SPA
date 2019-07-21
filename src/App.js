f0700b086ad7521940f55b9de7678fbb4380d9da
Fabiano Ramos <contato@fabianoramosd.dev>
2019-07-21 13:18:01 -0300

    Comentário Inicial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fabiano Ramos
+          Fabiano Ramos - SPA (Single Page Application)
         </p>
         <a
           className="App-link"
