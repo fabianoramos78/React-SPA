0b7e62a6fe56dd8a60a6ed6403038a7b07627ea0
Fabiano Ramos <contato@fabianoramosd.dev>
2019-07-21 16:47:56 -0300

    Componentização do App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/header/ButtonMob.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class ButtonMob extends React.Component {
+  render() {
+    return null;
+  }
+}
